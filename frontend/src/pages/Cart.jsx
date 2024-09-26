d6b09c4455e760d3b4c260c9f3685bacaa5e117d
Olaspace <eludiniolalekan@gmail.com>
2024-09-26 12:01:21 +0100

    first commit

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../context/CartContext";
 import axios from "axios";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe('pk_test_51PSwcqIhhZ6uv6X6w5NV1lqq4HR8jilNWFSAIVKOd01Gf9jhCNAXgCXeiJdjcz4QTNjSHv7aVDyzh1SAa9SfHe2h00ffxmX3fq');
+const stripePromise = loadStripe('pk_test_51Q3E9GBmSLmcvsS3LoIe1GLTltfJVRcHb8vr975nNvQFhyKKE2YMgwY5GPJwCGpHeB1XQdhuQJgiUoaJeBmPQXf1007Abxy2IH');
 
 const Cart = () => {
     const { cartItems, decreaseCartItemQuantity, addToCart, clearCart } = useCart();
